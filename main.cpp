5813ff73648902eb1b5d62c935e38d6e74f87c07
MohammadPourbakht <mohammad.poorbakhat.80@gmail.com>
2020-08-14 01:23:44 +0430

    add hiiiii

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
-
+#include <iostream>
 #include <QApplication>
+
+using namespace  std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    cout << "hiiiiiii "<< endl;
+
     // hellooooooooooooooooooooooooooooooooooooooo
     w.show();
     //mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
